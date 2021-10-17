22cc752c5dec215567b54e958b7b2928cb6e9e15
JonelSV <svjonel@gmail.com>
2021-10-17 18:36:14 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// npm init to create JSON package
+// npm i cheerio for Cheerio  = parses markup and provides an API for traversing/manipulating the resulting data structure.
+// npm i express (backend framework)
+// npm i axios (promise-based, http requests)
+
+const PORT = 8000;
+
